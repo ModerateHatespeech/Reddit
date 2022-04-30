8d172dbdf3bcaca357fc9a07b13c0ba6c16ae77b
Welton Wang <weltonwang88@gmail.com>
2022-04-30 15:05:17 -0700

    update files

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Reddit
 Automatically moderate subreddits via API
+
+## Purpose
+The following python script create a Reddit bot that will auto-report content flagged as "Toxic" by our API to the subreddit's moderators.
+
+## Requirements
+Install requirements:
+```apt-get install python3
+pip3 install praw requests
+```
+
+Required config.json file:
+```json
+{
+  "subreddit": "subreddit",
+  "client_id": "REDDIT_APPLICATION_CLIENT_ID",
+  "client_secret": "REDDIT_APPLICATION_CLIENT_SECRET",
+  "username": "REDDIT_BOT_ACCOUNT_USERNAME",
+  "password": "REDDIT_BOT_ACCOUNT_PASSWORD",
+  "api_token": "MODERATE_HATESPEECH_API_TOKEN",
+  "threshold": CONFIDENCE_THRESHOLD (float)
+}
+```
+
+The subreddit(s) to scan can be passed as a list of subreddits joined with a "+" -- for example, "reddit+politics+news." An example config file is provided below:
+```json
+{
+  "subreddit": "news+worldnews",
+  "client_id": "G23Ap9HxVzZGW9cvH68a",
+  "client_secret": "q-MScVnSE5i4UfnMucGZZs3avyX",
+  "username": "toxicitymodbot",
+  "password": "jH^cFU%V%Pf7&MMPA",
+  "api_token": "f7835af37818e44fa22b5393e3330811",
+  "threshold": 0.9
+}
+```
+
---
+++ main.py
@@ -0,0 +1,66 @@
+"""
+Toxicity Content Bot Script by ModerateHatespeech.com
+@description Scans subreddit(s) for content that is considered toxic and reports the comment to moderators
+@version 1.0.0
+@last_updated 2/13/2022
+"""
+
+import praw
+import requests
+import json
+import traceback
+import logging
+import re
+
+logging.basicConfig(filename='report.log', format='%(asctime)s - %(message)s', level=logging.INFO) # action log
+
+def load_config():
+  """ Load configuration file """
+  with open("config.json", "r") as f:
+    config = json.load(f)
+    missing = ["client_id", "client_secret", "subreddit", "api_token","username", "password","threshold"] - config.keys()
+    if len(missing) > 0:
+      raise KeyError("Missing keys in config.json {0}".format(str(missing)))
+    return config
+
+def login(config):
+  """ Login to Reddit """
+  reddit = praw.Reddit(
+      user_agent = "ToxicContentBot (by u/toxicitymodbot)",
+      client_id = config['client_id'],
+      client_secret = config['client_secret'],
+      password = config['password'],
+      username = config['username']
+  )
+  return reddit
+
+def moderate(text, thresh):
+  """ Call API and return response list with boolean & confidence score """
+  text = re.sub(r'>[^\n]+', "", text) # strip out quotes
+  response = requests.post("https://api.moderatehatespeech.com/api/v1/toxic/", json={"token":config['api_token'], "text":text}).json()
+
+  if response['response'] != "Success":
+    if response['response'] != "Authentication failure":
+      raise AttributeError('Invalid response: {0}'.format(response['response']))
+    else:
+      raise RuntimeError('Fatal response: {0}'.format(response['response']))
+
+  if response['class'] == "flag" and float(response['confidence']) > thresh:
+    return [True, round(float(response['confidence']), 3)]
+
+  return [False, round(float(response['confidence']), 3)]
+
+if __name__ == "__main__":
+  config = load_config()
+  reddit = login(config)
+  subreddit = reddit.subreddit(config['subreddit'])
+
+  for comment in subreddit.stream.comments():
+    try:
+      result = moderate(comment.body, config['threshold'])
+      if result[0]:
+        logging.info('Comment ({0}) reported @ {1}% confidence'.format(comment.permalink, result[1] * 100))
+        comment.report("Automatic report from u/{0} for toxicity @ {1}% confidence".format(config['username'], result[1] * 100))
+    except (AttributeError, KeyError):
+      logging.warning(traceback.format_exc())
+      traceback.print_exc()
